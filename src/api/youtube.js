ce0490b124cd3b2b6d37ff9de04aba3736922526
Alexander <ojubanirealex@gmail.com>
2022-10-15 05:54:28 +0100

    re-added youtube api file

---
+++ src/api/youtube.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+// Insert Api key string here
+// const KEY = Api_Key;
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY,
+  },
+});
